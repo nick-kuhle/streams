5bd2c622e2cc2cc52fe20af8b93a50cc3892c59e
Nick Kuhle <nickkuhle@yahoo.com>
2025-08-07 16:38:39 +0000

    Changed verbage... Again.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="font-sans grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-1 items-center sm:items-start">
         <div className="font-mono text-center text-xl">
-          USTA BJK Old Camera Live Streams
+          USTA BJK Nats Live (older cameras)
         </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
