f2431084644c5351f25bffc4f71a64a8a6eb0092
Nick Kuhle <nickkuhle@yahoo.com>
2025-08-07 15:41:12 +0000

    Updated courts to relevant cameras, updated old URLs to correct URLs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,139 +8,48 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=Stadium"
-            target="_blank"
-          >
-            Stadium
-          </a>          
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC01"
-            target="_blank"
-          >
-            Court 1 
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC02"
-            target="_blank"
-          >
-            Court 2 
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC03"
-            target="_blank"
-          >
-            Court 3 
-          </a>
-        </div>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC06"
-            target="_blank"
-          >
-            Court 6
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://ams.leit.us:5443/live/play.html?id=TC07"
             target="_blank"
           >
-            Court 7 
-          </a>
+            Court 7
+          </a>          
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://ams.leit.us:5443/live/play.html?id=TC09"
             target="_blank"
           >
-            Court 9 
+            Court 9
           </a>
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC10"
+            href="https://ams.leit.us:5443/OldCam/play.html?id=TC12"
             target="_blank"
           >
-            Court 10
+            Court 12
           </a>
         </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC12"
-            target="_blank"
-          >
-            Court 12
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC13"
+            href="https://ams.leit.us:5443/OldCam/play.html?id=TC13"
             target="_blank"
           >
             Court 13
           </a>
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC14"
-            target="_blank"
-          >
-            Court 14
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC15"
-            target="_blank"
-          >
-            Court 15
-          </a>
-        </div>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC16"
-            target="_blank"
-          >
-            Court 16
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC17"
+            href="https://ams.leit.us:5443/OldCam/play.html?id=aVIkUZMDQXCdL4YA154482568498307"
             target="_blank"
           >
             Court 17
           </a>
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC19"
+            href="https://ams.leit.us:5443/OldCam/play.html?id=TC18"
             target="_blank"
           >
-            Court 19
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC20"
-            target="_blank"
-          >
-            Court 20
-          </a>
-        </div>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC21"
-            target="_blank"
-          >
-            Court 21
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ams.leit.us:5443/live/play.html?id=TC22"
-            target="_blank"
-          >
-            Court 22
+            Court 18
           </a>
         </div>
       </main>
